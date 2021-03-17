c2756e4b9db705dace38a4c8bff848b69eb8b6ab
Elen Carvalho <60263754+Elen0207@users.noreply.github.com>
2021-03-16 22:41:53 -0300

    Exercício pronto
    Deixei o código mais enxuto.

--- AtividadeThreadNaAula/src/controller/Controller.java
+++ AtividadeThreadNaAula/src/controller/Controller.java
@@ -1,25 +1,18 @@
 package controller;
+
 import java.util.concurrent.Semaphore;
-
 public class Controller extends Thread {
-    private int [] pessoas = new int [300];;
-    private int compra = 0;
-    private int venda;
     private Semaphore semaforo;
+    private int ingressos = 100;
 
-    public Controller (int [] pessoas, int compra, int venda, Semaphore semaforo) {
-        this.pessoas = pessoas;
-        this.compra = compra;
-        this.venda = venda;
+    public Controller ( Semaphore semaforo) {
         this.semaforo = semaforo;
     }
 
     public void run () {
         try {
             semaforo.acquire();
-            Login(pessoas, compra);
-            Processo(pessoas, compra);
-            Validacao(pessoas, compra);
+            Validacoes();
         } 
         catch (InterruptedException e) {
 		    e.printStackTrace();
@@ -29,81 +22,53 @@ public class Controller extends Thread {
 		} 
     }
 
-    public int Login (int [] pessoas, int compra) {
-       
 
-        for ( int i = 0; i <= 300; i ++) {
+    public void Validacoes () {      
+        
+        for ( int pessoas = 0; pessoas<= 300; pessoas ++) {
             int tempo = (int)(Math.random() * 2000 + 500);
+            
+            try {
+                sleep(tempo);
+            } 
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
             if ( tempo > 1000 ) {
-                    try {
-                        sleep(tempo);
-                    } 
-                    catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                System.out.println("Erro na compra. Deu timeout");
+                System.err.println("Erro na compra. Deu timeout");
             }
             else {
-                    try {
-                        sleep(tempo);
-                    } 
-                    catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    compra ++;
-                    pessoas[i] = pessoas[compra];
-                    Processo(pessoas, compra);
-            }
-        }
-        return compra;
-    }
-
-    public int Processo(int [] pessoas, int compra) {
-        
-        for ( int i = 0; i <= 300; i ++) {
-            int tempo = (int)(Math.random() * 3000 + 1000);
-
-            if ( tempo > 2500 ) {
+                System.out.println("A pessoa #"+pessoas+" se logou");
+                tempo = (int)(Math.random() * 3000 + 1000);
+            
                 try {
                     sleep(tempo);
                 } 
-                catch (InterruptedException e) {
+                    catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                compra --;
-                if (compra < 0 ) {
-                    compra = 0;
-                }
-                System.out.println("Erro na compra. Deu timeout");
-            }
-            else {
-                try {
-                    sleep(tempo);
-                } 
-                catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                pessoas[i] = pessoas[compra];
-                Validacao(pessoas, compra);
-            }
-        }
-        return compra;
-    }
 
-
-    public void Validacao (int [] pessoas, int compra) {
-        int ingressos = 0;
-        for ( int i = 0; i <= 300; i ++) {
-            while( ingressos > compra ) {
-                venda = (int)(Math.random() * 4 + 1);
-                if ( ingressos <= 100 ) { 
-                    ingressos = ingressos + venda;
-                    System.out.println("A pessoa #"+pessoas[i]+" comprou "+venda+" ingressos.\n"
-                                            +"Ainda tem "+ingressos+"ingressos disponíveis.");;
+                if ( tempo > 2500 ) {
+                    System.err.println("Erro na compra. Deu timeout");
                 }
                 else {
-                    System.out.println("Os ingressos acabaram!");
+
+                    System.out.println("A pessoa #"+pessoas+" passou pelo processo de compra");
+
+                    while( ingressos > 0 ) {
+                        
+                        int venda = (int)(Math.random() * 4 + 1);
+                        ingressos = ingressos - venda;
+        
+                        if ( ingressos > 0 ) { 
+                            System.out.println("A pessoa #"+pessoas+" comprou "+venda+" ingressos.\n"
+                                                    +"Ainda tem "+ingressos+" ingressos disponíveis.");;
+                        }
+                        else {
+                            System.out.println("Os ingressos acabaram!");
+                        }
+                    }
                 }
             }
         }
--- AtividadeThreadNaAula/src/view/Main.java
+++ AtividadeThreadNaAula/src/view/Main.java
@@ -5,12 +5,10 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        int [] pessoas = new int [300];
-        int compra = 0, venda = 0;
         Semaphore semaforo = new Semaphore(1); 
 
-        for (int id = 1; id < 300; id ++) {
-            Thread thread = new Controller(pessoas, compra, venda, semaforo);
+        for (int i = 0; i < 300; i ++) {
+            Thread thread = new Controller(semaforo);
             thread.start();
         }
     }
